4f1eb103afdd10f0a002a9b979605ac007890714
Caio Ricciardi <cricciardi@lymtech.com.br>
2022-02-21 08:36:13 -0300

    att backup

--- lym_portal/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import myportal
-from . import agendamento
-from . import formsagendamento
-from . import servico
-from . import filapadrao
-from . import filaprioridade
